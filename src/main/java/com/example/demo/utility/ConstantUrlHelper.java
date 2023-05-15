efc16f391b7e59766d5da2e2b325ce83cde15e80
Shohaib Shaikh <shohaibxshaikh@gmail.com>
2023-05-15 11:46:45 +0530

    Added some constant variables

--- src/main/java/com/example/demo/utility/ConstantUrlHelper.java
+++ src/main/java/com/example/demo/utility/ConstantUrlHelper.java
@@ -4,6 +4,8 @@ public class ConstantUrlHelper {
 
     public static final String USER_URL = "/user";
     public static final String PRODUCT_URL = "/product";
+    public static final String ORDER_URL = "/order";
+    public static final String CREATE_URL = "/create";
     public static final String MESSAGE_URL = "/message";
     public static final String USER_MESSAGE_URL = "/user/message";
 }
